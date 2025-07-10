6ca53fe0057e0eeae7f1f14b5d2771d04b3bb616
Ravikant singh <sravikant302@gmail.com>
2025-07-11 00:30:43 +0530

    Added new feature or code update

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -9,9 +9,10 @@ function Body() {
       <div className='search'>Search</div>
 
      <div className="res-container">
-      <RestaurantCard resName="Maghana foods" cuisine="Biryani,North Indian, Asian"/>
-       <RestaurantCard resName="KFC" cuisine="burgur , fast food"/>
-      
+      <RestaurantCard resName="Maghana foods" cuisine="Biryani,North Indian, Asian" stars="2-star" minites="25 minites"/>
+       <RestaurantCard resName="KFC" cuisine="burgur , fast food" stars="3 star" minites="50 minites"/>
+        <RestaurantCard resName="Maghana foods" cuisine="Biryani,North Indian, Asian" stars="2-star" minites="25 minites"/>
+       <RestaurantCard resName="KFC" cuisine="burgur , fast food" stars="3 star" minites="50 minites"/>
      </div>
         
       </div>
--- src/Component/RestaurantCard.js
+++ src/Component/RestaurantCard.js
@@ -12,8 +12,8 @@ function RestaurantCard(props) {
 <img className="res-logo"alt='res-logo' src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/5/22/a7212f44-c5fa-4d6a-a6d3-5dca7dee467e_1099688.jpg'/>
      <h3>{props.resName}</h3>
   <h4>{props.cuisine}</h4>
-  <h4>4.4 stars</h4>
-  <h4>38 minutes</h4>
+  <h4>{props.stars}</h4>
+  <h4>{props.minites}</h4>
     </div>
   )
 }
